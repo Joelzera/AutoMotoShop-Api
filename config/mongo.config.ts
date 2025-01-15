71b695eabe217c425ba3fa44f0397712eda3453d
joel bispo <joelbispo04@hotmail.com>
2025-01-15 10:57:25 -0300

    feito a refatoração da conexão com o mongodb e rota de post

--- config/mongo.config.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('localhost:27017/auto_moto')
-const database = mongoose.connection
-
-export default database
--- src/routes/server.ts
+++ src/routes/server.ts
@@ -1,9 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import database from '../../config/mongo.config'
 import { execute } from '../../config/mysql.config'
 import cors from 'cors'
-import dotenv from 'dotenv'
+import car from '../models/car'
+import mongoose from 'mongoose';
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cors())
 
 
+mongoose.connect('mongodb://localhost:27017/auto_moto')
+
+
 app.post('/user', async (req, res) => {
     try {
         const query = 'INSERT INTO users (email, password) VALUES (?,?)'
@@ -20,7 +23,7 @@ app.post('/user', async (req, res) => {
             req.body.email,
             req.body.password
         ])
-        
+
         console.log(result)
         res.send(result)
     } catch (error) {
@@ -28,6 +31,26 @@ app.post('/user', async (req, res) => {
     }
 })
 
+app.post('/car', async (req, res) => {
+    const dataCar =  new car({
+        nome: req.body.nome,
+        marca: req.body.marca,
+        ano: req.body.ano,
+        modelo: req.body.modelo,
+        tipo: req.body.tipo,
+        cor: req.body.cor,
+        cambio: req.body.cambio,
+        combustivel: req.body.combustivel,
+        quilometragem: req.body.quilometragem
+    })
+    try {
+        const dataSaveCar =  await dataCar.save()
+        res.status(200).json(dataSaveCar)
+        console.log(dataSaveCar)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
